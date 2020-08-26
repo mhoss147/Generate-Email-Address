0a66cbf2d920e53beb9c469220b9630c1ad1fc64
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-08-26 17:42:25 -0400

    Private method setDepartment added

--- Email.java
+++ Email.java
@@ -1,6 +1,7 @@
 package emailApp;
 
 import java.lang.reflect.Constructor;
+import java.util.Scanner;
 
 public class Email {
 	
@@ -23,16 +24,36 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		
-		// Print to console
 		
+		// Print to console		
 		System.out.println("Email created: "+ this.firstName + " " + this.lastName);
+		
+		
+		// Call a method asking for dept and return the dept
+		this.department = setDepartment();		
 		
 	}
 	
 	
 	
 	
-	// Ask for the department
+	// Ask for the department, Don't want a public method
+	private String setDepartment() {
+		
+		System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		
+		// Read the input		
+		Scanner in = new Scanner(System.in);
+		
+		int deptChoice = in.nextInt();
+		
+		if (deptChoice == 1) { return "Sales";}
+		else if (deptChoice == 2) { return "Development"; }
+		else if (deptChoice == 3) { return "Accounting"; }
+		else {	return ""; }
+		
+		
+	}
 	
 	
 	// Generate the random password
