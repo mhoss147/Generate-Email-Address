a2614bf1f6dcee326524f1b32b6624ddc65e9e51
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-08-26 17:06:57 -0400

    Email class created,

---
+++ Email.java
@@ -0,0 +1,19 @@
+package emailApp;
+
+import java.lang.reflect.Constructor;
+
+public class Email {
+	
+	// Private variables, don't want anyone to access!
+	
+	private String firstName; 
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailBoxCapacity;
+	private String alternateEmail;
+	
+	
+	
+
+}
